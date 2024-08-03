fb071d659856f0a9881505f26b76570a3a4da796
likunqi <18800566899@163.com>
2024-08-03 23:21:55 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -50,7 +50,7 @@ export default defineUserConfig({
       mountPath: "/",
       // 这里使用 fileUrlTreeAnalysis 文件放到对应的文件路径中
       analysis: fileUrlTreeAnalysis({
-        "/云易搜.app": "https://github.com/likunqi/FList/releases/download/untagged-315df80791133bc82e3b/default.apk",
+        "/123.apk": "https://github.com/likunqi/FList/releases/download/app/123.apk",
         // "/文件树测试/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
         // "/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm"
       }),
